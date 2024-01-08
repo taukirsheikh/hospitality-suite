053843dc4f3587676589cb208ac5d47e9c9d3328
taukirsheikh <taukirsheikh2@gmail.com>
2024-01-08 14:28:42 +0545

    theme provider updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
+import ThemeProvider from '@/components/ThemeProvider/ThemeProvider'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '700', '900'], style: ['italic', 'normal'], variable: "--font-poppins", })
 
@@ -19,12 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main className='font-normal'>
-          <Header />
+        <ThemeProvider>
+          <main className='font-normal'>
+            <Header />
 
-          {children}
-          <Footer />
-        </main>
+            {children}
+            <Footer />
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { useEffect, useState } from "react"
-import ThemeContext from "@/context/ThemeContext"
+import { useEffect, useState } from "react";
+import ThemeContext from "@/context/ThemeContext";
+import { redirect } from "next/dist/server/api-utils";
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
     const localStorageAvailable = typeof localStorage !== "undefined";
@@ -8,19 +9,27 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
         ? JSON.parse(localStorage.getItem("hotel-theme")!) : false
 
 
-    const [darkTheme, setDarkTheme] = useState<boolean> (themeFromStorage);
+    const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
+    const [renderComponent, setRenderComponent]=useState(false);
+    useEffect(() => {
+        setRenderComponent(true);
+        
+    }, []);
+    if (!renderComponent) return <></>;
 
-        <ThemeContext.Provider  value = {{ darkTheme, setDarkTheme }
-}>
-    <div className={`${darkTheme ? 'dark' : ""} min-h-screen`}>
-        <div className="dark:text-white dark:bg-black text-[#1E1E1E]">
+    return (
+        <ThemeContext.Provider value={{ darkTheme, setDarkTheme }
+        }>
 
-            {children}
-        </div>
+            <div className={`${darkTheme ? 'dark' : ""} min-h-screen`}>
+                <div className="dark:text-white dark:bg-black text-[#1E1E1E]">
+                    {children}
+                </div>
+            </div>
+        </ThemeContext.Provider>
+    );
 
-    </div>
-      </ThemeContext.Provider>
 
-
-}
+};
+export default ThemeProvider;
 // const themeFromStorage = typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme") ? JSON.parse(localStorage.getItem("hotel-theme")!) : false
