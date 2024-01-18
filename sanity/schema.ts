07a285d72d87d72e69e456003cdaf632a9a9a298
taukirsheikh <taukirsheikh2@gmail.com>
2024-01-18 14:44:28 +0545

    sanity schema created

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,11 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import user from "./schemas/user";
+import booking from "./schemas/booking";
+import hotelRoom from "./schemas/hotelRoom";
+import review from "./schemas/review";
+import verificationToken from "./schemas/verificationToken";
+import account from "./schemas/account";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [user, account, booking, hotelRoom, review, verificationToken],
+};
